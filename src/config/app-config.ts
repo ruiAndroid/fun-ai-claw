6ae9610353251ebc74293b357489e0204a1e8fba
jianrui <jianrui@fun.tv>
2026-03-10 13:55:02 +0800

    1

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
   controlApiBaseUrl: "/fun-claw/api",
   uiControllerBaseUrl: "http://8.152.159.249:80/fun-claw/ui-controller",
-  defaultHostId: "00000000-0000-0000-0000-000000000098",
+  defaultHostId: "00000000-0000-0000-0000-0000000000108",
 } as const;

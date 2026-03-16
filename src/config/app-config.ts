db540b0fa879ef110821467820123f824f4b9b92
jianrui <jianrui@fun.tv>
2026-03-16 16:33:00 +0800

    运维接口收紧2

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
   controlApiBaseUrl: "/fun-claw/api",
-  uiControllerBaseUrl: "/fun-claw/ops/ui-controller",
+  uiControllerBaseUrl: "/fun-claw/api/ops/ui-controller",
   defaultHostId: "00000000-0000-0000-0000-000000000108",
 } as const;
